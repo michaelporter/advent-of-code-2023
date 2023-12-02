a25598ef8f28cf87f0aec0a01371cfc42ae32d51
michaelporter <michael@teepublic.com>
2023-12-01 21:06:26 -0500

    day one part 2

--- advent/lib/advent/solution/day_1.ex
+++ advent/lib/advent/solution/day_1.ex
@@ -1,10 +1,8 @@
 defmodule Advent.Solution.Day1 do
 
   def part_one do
-    [
-      0 | get_problem_input()
-    ]
-    |> Enum.reduce(fn row, sum ->
+    get_problem_input()
+    |> Enum.reduce(0, fn row, sum ->
       digits = Regex.scan(~r/\d/, row)
 
       calibration_number = Enum.join([
@@ -16,18 +14,83 @@ defmodule Advent.Solution.Day1 do
     end)
   end
 
-
   ###
 
-  # this doesn't get the eight out of  "oneight"
-  # but tbh I'm not sure if I need to get the text ones out just yet
-  #parsed = Regex.scan(~r/one|two|three|four|five|six|seven|eight|nine|\d/U, calibration_string)
   def part_two do
+    get_problem_input()
+    |> Enum.reduce(0, fn calibration_string, sum ->
+      calibration_number = extract_digits_from_number_words(calibration_string)
+      |> extract_literal_digits(calibration_string)
+      |> Enum.sort(&sort_by_index/2)
+      |> get_calibration_number
 
+      sum + calibration_number
+    end)
   end
 
 
   ###
+
+  defp get_calibration_number(digits) do
+    {first, _} = Enum.at(digits, 0)
+    {last, _} = Enum.at(digits, -1)
+
+    "#{first}#{last}" |> String.to_integer()
+  end
+
+  defp extract_literal_digits(digits, calibration_string) do
+    literal_digits = Regex.scan(~r/\d/, calibration_string, return: :index)
+
+    Enum.reduce(literal_digits, digits, fn literal_digit, d ->
+      [{index, _}] = literal_digit
+      parsed_digit = String.at(calibration_string, index)
+
+      [{parsed_digit, index} | d]
+    end)
+  end
+
+  defp extract_digits_from_number_words(calibration_string) do
+    number_words = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine"
+    ]
+
+    Enum.reduce(number_words, [], fn number_word, digits ->
+      matches = Regex.scan(~r/(#{number_word})/, calibration_string, return: :index)
+
+      Enum.reduce(matches, digits, fn [{index, _}, _], acc ->
+        [{number_word_int_str(number_word), index} | acc ]
+      end)
+    end)
+  end
+
+  defp sort_by_index(a, b) do
+    {_, index_a} = a
+    {_, index_b} = b
+
+    index_a < index_b
+  end
+
+  defp number_word_int_str(number_word) do
+    case number_word do
+      "one" -> "1"
+      "two" -> "2"
+      "three" -> "3"
+      "four" -> "4"
+      "five" -> "5"
+      "six" -> "6"
+      "seven" -> "7"
+      "eight" -> "8"
+      "nine" -> "9"
+    end
+  end
 
   defp parse_response_body(body) do
     String.trim_trailing(body)
