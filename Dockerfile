a1b8af2858db536bef32ee015a80ccef1c1a10de
michaelporter <michael@teepublic.com>
2023-12-01 19:57:59 -0500

    cleanup + day one part 1; docker not working

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM elixir:latest
 # FROM elixir-base
+# FROM elixir:1.12.3
 
 # CMD echo "Test - we're in!"
 WORKDIR /elixir-app
@@ -8,7 +9,7 @@ WORKDIR /elixir-app
 COPY . .
 # docker run -it elixir-env-1 iex
 
-RUN apt-get update && apt-get install inotify-tools -y
+RUN apt-get update && apt-get install inotify-tools erlang-dev erlang-xmerl -y
 
 RUN mix local.hex --force
 
@@ -22,7 +23,7 @@ RUN mix local.rebar --force
 
 EXPOSE 4000
 
-# CMD ["elixir", "index.exs"]_
+# CMD ["elixir", "index.exs"]
 
 CMD ["mix", "phx.server"]
 # CMD ["iex", "-S", "mix", "phx.server"]
--- advent/lib/advent/solution/day_1.ex
+++ advent/lib/advent/solution/day_1.ex
@@ -1,12 +1,27 @@
 defmodule Advent.Solution.Day1 do
 
   def part_one do
+    [
+      0 | get_problem_input()
+    ]
+    |> Enum.reduce(fn row, sum ->
+      digits = Regex.scan(~r/\d/, row)
 
+      calibration_number = Enum.join([
+        Enum.at(digits, 0),
+        Enum.at(digits, -1)
+      ]) |> String.to_integer()
+
+      sum + calibration_number
+    end)
   end
 
 
   ###
 
+  # this doesn't get the eight out of  "oneight"
+  # but tbh I'm not sure if I need to get the text ones out just yet
+  #parsed = Regex.scan(~r/one|two|three|four|five|six|seven|eight|nine|\d/U, calibration_string)
   def part_two do
 
   end
@@ -16,11 +31,9 @@ defmodule Advent.Solution.Day1 do
 
   defp parse_response_body(body) do
     String.trim_trailing(body)
-    |> String.split("\n\n")
-    |> Enum.map(fn elf ->
-      String.split(elf, "\n")
-      |> Enum.map(fn item -> String.to_integer(item) end)
-    end)
+    # |> String.split("\n\n")
+    |> String.split("\n", trim: true)
+
   end
 
   defp get_problem_input do
